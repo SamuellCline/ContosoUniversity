238634692f9f3666a57281cf957bd6ba14ff7759
Samuel Cline <samuellcline@gmail.com>
2019-03-30 12:36:25 -0600

    first commit

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -9,6 +9,11 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace ContosoUniversity
 {
@@ -24,16 +29,22 @@ namespace ContosoUniversity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
         }
+        //services.Configure<CookiePolicyOptions>(options =>
+        //{
+        //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        //    options.CheckConsentNeeded = context => true;
+        //    options.MinimumSameSitePolicy = SameSiteMode.None;
+        //});
+
+
+
+        //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
